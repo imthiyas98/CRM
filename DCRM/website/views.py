b0cad0a1aa53154189fdfa9818c7d2fbdc34eed9
imthiyas <imthiyas098@gmail.com>
2025-03-17 12:11:22 +0530

    LOGIN AND LOGOUT

--- DCRM/website/views.py
+++ DCRM/website/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
-    return render(request,'home.html')
-def login(request):
-    return HttpResponse("pls login")
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been logged in")
+                return redirect('home')
+            else:
+                messages.success(request, "There was an error logging in, please try again...")
+                return redirect('home')
+        else:
+            return render(request, 'home.html')
+        
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('home')
