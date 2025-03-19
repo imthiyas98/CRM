1b71dfa9563c396acc16b7a0f298da85500e9b75
imthiyas <imthiyas098@gmail.com>
2025-03-19 13:31:58 +0530

    created front table

--- DCRM/website/views.py
+++ DCRM/website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
+from.models import Record
 
 
 def home(request):
+        records = Record.objects.all()
         if request.method == 'POST':
             username = request.POST['username']
             password = request.POST['password']
@@ -17,9 +20,25 @@ def home(request):
                 messages.success(request, "There was an error logging in, please try again...")
                 return redirect('home')
         else:
-            return render(request, 'home.html')
+            return render(request, 'home.html',{'records': records})
         
 def logout_user(request):
     logout(request)
-    messages.success(request, "You have been logged out...")
+    messages.success(request, "You have been logged out!!")
     return redirect('home')
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html' ,{'form':form})
